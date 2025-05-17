fdde215e526eb3bb4c471b072842d2ab8e217722
B1lol-dev <sardorovichgamer@gmail.com>
2025-05-17 15:38:49 +0500

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import Header from "@/components/layout/Header";
+import AddUser from "./_components/AddUser";
+
 export default function Home() {
-  return <>Hello World</>;
+  return (
+    <>
+      <Header />
+      <main className="bg-gray-300 py-5">
+        <AddUser />
+      </main>
+    </>
+  );
 }
