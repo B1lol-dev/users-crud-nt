9db29cf4f9c60db50bb75a98537dd035b7eeca32
B1lol-dev <sardorovichgamer@gmail.com>
2025-05-17 18:33:22 +0500

    added users to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@
 
 import Header from "@/components/layout/Header";
 import AddUser from "./_components/AddUser";
+import Users from "./_components/Users";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="bg-gray-300 py-5">
+      <main className="bg-gray-200 py-5">
         <AddUser />
+        <Users />
       </main>
     </>
   );
